03dea657d507522482f834c5bc65b514f90fddd7
Wong Kin Ting <strisen@Wongs-MacBook-Pro.local>
2018-01-27 19:04:08 +0800

    Functional build for simple payments

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,82 +1,40 @@
 class ChargesController < ApplicationController
-  before_action :amount_to_be_charged
-  before_action :authenticate_user!
-  before_action :description
+  before_action :amount_to_be_charged, :authenticate_user!
   def new
-
   end
 
   def create
-      @amount = params[:amount]
-      # @amount = @amount.gsub('$', '').gsub(',', '')
-      #
-      # begin
-      #   @amount = Float(@amount).round(2)
-      #   p @amount
-      # rescue
-      #   flash[:error] = 'Charge not completed. Please enter a valid amount in SGD$.'
-      #   redirect_to root_path
-      #   return
-      # end
-      #
-      @amount = (@amount * 100).to_i
-      # if @amount < 100
-      #   flash[:error] = 'Charge not completed. Amount entered must be greater than $1.'
-      #   redirect_to root_path
-      #   return
-      # end
 
-      customer = Stripe::Customer.create(
-        :email => params[:stripeEmail],
-        :source => params[:stripeToken]
+    customer = StripeTool.create_customer(email: params[:stripeEmail],
+                                          stripe_token: params[:stripeToken])
+
+    charge = StripeTool.create_charge(customer_id: customer.id,
+                                      amount: @amount,
+                                      description: "E-Credit")
+
+
+# Update database if transaction is unique and successful
+    @new_charge_id = charge.id
+    if !(Transaction.find_by(charge_id: "#{@new_charge_id}"))
+      @receipt = Stripe::Charge.retrieve(charge.id)
+      @user_balance = current_user.balance.to_f
+      @user_balance = @user_balance + (@receipt.amount/100)
+      current_user.balance = @user_balance
+      current_user.save
+      @new_transaction = Transaction.new(
+        user_id: current_user.id,
+        charge_id: @new_charge_id
       )
-
-      charge = Stripe::Charge.create(
-        :amount => @amount,
-        :currency => 'sgd',
-        :source => params[:stripeToken],
-        :description => @description
-      )
-      # Update database if transaction is unique and successful
-      # @new_charge_id = charge.id
-      # if (Transaction.find_by(charge_id: "#{@new_charge_id}"))
-      #   @receipt = Stripe::Charge.retrieve(charge.id)
-      #   @user_balance = current_user.balance.to_f
-      #   @user_balance = @user_balance + (@receipt.amount/100)
-      #   current_user.balance = @user_balance
-      #   current_user.save
-      #   @new_transaction = Transaction.new(
-      #     user_id: current_user.id,
-      #     charge_id: @new_charge_id
-      #   )
-      #   @new_transaction.save
-      # end
-      redirect_to root_path
-
-      rescue Stripe::CardError => e
-        flash[:error] = e.message
-        redirect_to edit_user_registration_path
-
-
-
-    # customer = StripeTool.create_customer(email: params[:stripeEmail],
-    #                                       stripe_token: params[:stripeToken])
-    #
-    # charge = StripeTool.create_charge(customer_id: customer.id,
-    #                                   amount: (params[:amount].to_f*100).to_i,
-    #                                   description: @description)
-
-
-
-    # redirect_to edit_user_registration_path
-    # rescue Stripe::CardError => e
-    # flash[:error] = e.message
-    # redirect_to edit_user_registration_path
+      @new_transaction.save
+    end
+    redirect_to edit_user_registration_path
+    rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to new_charge_path
   end
 
   def thanks
     # render json: params[:charge]
-
     # @new_charge_id = params[:charge]
     #
     # if !(Transaction.find_by(charge_id: "#{@new_charge_id}"))
@@ -91,7 +49,7 @@ class ChargesController < ApplicationController
     #   )
     #   @new_transaction.save
     # end
-    # redirect_to edit_user_registration_path
+    # redirect_to root_path
 
     #code
     # if Stripe::Charge.list().data.first.source.name == current_user.email
@@ -101,4 +59,25 @@ class ChargesController < ApplicationController
     # render json: @receipt
     # can get
   end
+
+  # def description
+  #   @description = "Some amazing product"
+  # end
+  #
+  def amount_to_be_charged
+    @amount = params[:amount]
+    @amount = @amount.gsub('$','').gsub(',', '')
+    begin
+      @amount = Float(@amount).round(2)
+    rescue
+      flash[:error] = 'Error in Amount'
+    end
+
+    @amount = params[:amount].to_f*100
+    @amount = @amount.to_i
+  end
+  #
+  # def deposit_params
+  #   params.require(:charge).permit(:user)
+  # end
 end
