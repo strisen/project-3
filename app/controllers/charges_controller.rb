662d39096a017e01dfb6a5b0872fea0544bd006d
Wong Kin Ting <strisen@Wongs-MacBook-Pro.local>
2018-01-27 16:43:14 +0800

    New build with customisable form

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -4,19 +4,40 @@ class ChargesController < ApplicationController
   end
 
   def create
-    return redirect_to edit_user_registration_path if /a-zA-Z/.match(params[:amount])
 
-    customer = StripeTool.create_customer(email: params[:stripeEmail],
-                                          stripe_token: params[:stripeToken])
+    @amount = params[:amount]
+    @amount = @amount.gsub('$', '').gsub(',', '')
 
-    charge = StripeTool.create_charge(customer_id: customer.id,
-                                      amount: (params[:amount].to_f*100).to_i,
-                                      description: @description)
+    begin
+      @amount = Float(@amount).round(2)
+    rescue
+      flash[:error] = 'Charge not completed. Please enter a valid amount in SGD$.'
+      redirect_to edit_user_registration_path
+      return
+    end
 
+    @amount = (@amount * 100).to_i
+    if @amount < 100
+      flash[:error] = 'Charge not completed. Amount entered must be greater than $1.'
+      redirect_to edit_user_registration_path
+      return
+    end
 
-# Update database if transaction is unique and successful
+    customer = Stripe::Customer.create(
+      :email => params[:stripeEmail],
+      :source => params[:stripeToken]
+    )
+
+    charge = Stripe::Charge.create(
+      :amount => @amount,
+      :currency => 'sgd',
+      :source => params[:stripeToken],
+      :description => @description,
+      :customer => customer_id
+    )
+    # Update database if transaction is unique and successful
     @new_charge_id = charge.id
-    if !(Transaction.find_by(charge_id: "#{@new_charge_id}"))
+    if (Transaction.find_by(charge_id: "#{@new_charge_id}"))
       @receipt = Stripe::Charge.retrieve(charge.id)
       @user_balance = current_user.balance.to_f
       @user_balance = @user_balance + (@receipt.amount/100)
@@ -29,28 +50,45 @@ class ChargesController < ApplicationController
       @new_transaction.save
     end
     redirect_to edit_user_registration_path
+
     rescue Stripe::CardError => e
-    flash[:error] = e.message
-    redirect_to edit_user_registration_path
+      flash[:error] = e.message
+      redirect_to edit_user_registration_path
+
+
+    # customer = StripeTool.create_customer(email: params[:stripeEmail],
+    #                                       stripe_token: params[:stripeToken])
+    #
+    # charge = StripeTool.create_charge(customer_id: customer.id,
+    #                                   amount: (params[:amount].to_f*100).to_i,
+    #                                   description: @description)
+
+
+
+    # redirect_to edit_user_registration_path
+    # rescue Stripe::CardError => e
+    # flash[:error] = e.message
+    # redirect_to edit_user_registration_path
   end
 
   def thanks
     # render json: params[:charge]
-    @new_charge_id = params[:charge]
 
-    if !(Transaction.find_by(charge_id: "#{@new_charge_id}"))
-      @receipt = Stripe::Charge.retrieve(params[:charge])
-      @user_balance = current_user.balance.to_f
-      @user_balance = @user_balance + (@receipt.amount/100)
-      current_user.balance = @user_balance
-      current_user.save
-      @new_transaction = Transaction.new(
-        user_id: current_user.id,
-        charge_id: @new_charge_id
-      )
-      @new_transaction.save
-    end
-    redirect_to edit_user_registration_path
+    # @new_charge_id = params[:charge]
+    #
+    # if !(Transaction.find_by(charge_id: "#{@new_charge_id}"))
+    #   @receipt = Stripe::Charge.retrieve(params[:charge])
+    #   @user_balance = current_user.balance.to_f
+    #   @user_balance = @user_balance + (@receipt.amount/100)
+    #   current_user.balance = @user_balance
+    #   current_user.save
+    #   @new_transaction = Transaction.new(
+    #     user_id: current_user.id,
+    #     charge_id: @new_charge_id
+    #   )
+    #   @new_transaction.save
+    # end
+    # redirect_to edit_user_registration_path
 
     #code
     # if Stripe::Charge.list().data.first.source.name == current_user.email
