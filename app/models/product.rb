938c78e39e097aa57d92b43f388328d9029d5b26
Wong Kin Ting <strisen@Wongs-MacBook-Pro.local>
2018-01-30 11:56:53 +0800

    Removed carts stuff

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,22 +8,10 @@ class Product < ApplicationRecord
     @current_user.purchases.create(product_id: self.id)
   end
 
-  def in_cart(user_id)
-    @product = Product.find(self.id)
-    @cart_item = @product.cart_item.where(user_id: user_id)
-    @cart_item.any?
-  end
-
-  def cart_id (user_id)
-    @product = Product.find(self.id)
-    @cart_item = @product.cart_item.where(user_id: user_id)
-    @cart_id = @cart_item[0].id
-  end
-
   def change_status(stock_status_id)
     @product = Product.find(self.id)
-
   end
+
   def product_id
     @product = Product.find(self.id)
   end
