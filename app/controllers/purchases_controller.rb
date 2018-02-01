b98d9380c36ed1ddc7614bd5257f0b7422209450
Wong Kin Ting <strisen@Wongs-MacBook-Pro.local>
2018-02-01 19:26:52 +0800

    Final build for fixed CSS

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -6,8 +6,10 @@ class PurchasesController < ApplicationController
   end
 
   def show
-     @current_purchase = current_user.purchases.last.product_id
-     @latest_purchase = Purchase.find(params[:id]) # .find method only by id
+    @purchase = current_user.purchases.last.product_id
+    @current_purchase = Product.find(@purchase)
+    # render json: @current_purchase
+    @latest_purchase = Purchase.find(params[:id]) # .find method only by id
   end
 
   def new
