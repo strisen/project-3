d6898b2fc939dc65e0799deb5dfea21266a46ef7
Wong Kin Ting <strisen@Wongs-MacBook-Pro.local>
2018-02-01 19:55:24 +0800

    Final Build for Submission

--- config/routes.rb
+++ config/routes.rb
@@ -3,32 +3,16 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
   resources :charges, only: [:create, :index]
-  # root to: 'posts#index'
 
-  # get 'charges/new'
-  #
-  # get 'charges/create'
-
-  # get 'pages/home'
   root 'pages#home'
 
-  # get 'pages/about'
   get '/about', to: 'pages#about'
 
-  # get 'pages/marketplace'
-  # get '/marketplace', to: 'pages#marketplace'
-
-  # get 'pages/login'
-  # get '/login', to: 'pages#login'
   resources :products
 
   resources :purchases
 
   resources :approvals
-  # post 'pages/approved'
-  # post '/approved', to: 'pages#approved'
-  # post 'pages/rejected'
-  # post '/rejected', to: 'pages#rejected'
 
   # devise routes
   devise_for :users,
@@ -40,14 +24,6 @@ Rails.application.routes.draw do
       registration: '',
       sign_up: 'register'
     }
-
-
-  # Rails.application.routes.draw do
-
-  # end
-
-  # get 'thanks', to: 'charges#thanks', as: 'thanks'
-
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
